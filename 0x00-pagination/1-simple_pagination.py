7b583f7cf7ed2ae8d0bbdcb2789a1e3e2aa15f9e
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-18 20:39:37 +0000

    feat: implemented a simple class and function to showcase how pagination works

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    A function that takes two integers and returns tuple of size two containing
+    a start index and an end index.
+    """
+    end = page * page_size
+    start = end - page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        A function that returns the list of pages per the requested current
+        page and the page sizes.
+        """
+        assert type(page) is int
+        assert type(page_size) is int
+        assert page > 0
+        assert page_size > 0
+
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        if end <= len(dataset):
+            return dataset[start:end]
+        else:
+            return []
