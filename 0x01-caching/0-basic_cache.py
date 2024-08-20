af239e3a5dd80ca8180e22234691a1aa2d2e1fb6
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-20 17:57:19 +0000

    feat: done with implementing task 0, also writing the readme

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A class implements a basic caching system
+    """
+    def __init__():
+        pass
