cb5f80a0cf87e19f6c8ff74962c7170f4295983b
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-21 10:50:31 +0000

    fix: Made a few changes to fix a few errors

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+A module to represent a caching system
 """
 from base_caching import BaseCaching
 
@@ -9,5 +9,5 @@ class BasicCache(BaseCaching):
     """
     A class implements a basic caching system
     """
-    def __init__():
-        pass
+    def __init__(self):
+        super().__init__()
