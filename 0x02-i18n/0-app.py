ba987f7c87f4da313372fcdd48427b5a697f2b87
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-09 09:36:44 +0000

    feat: had to rename the index.html

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask Application
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """A route function for the index page
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
